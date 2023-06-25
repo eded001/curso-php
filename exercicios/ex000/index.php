9438a526b7bc0c7d8016db56147e6391d25f04a9
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2023-06-25 19:57:50 -0300

    novo index para o ex000

---
+++ exercicios/ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style-principal.css">
+    <title>hello world</title>
+</head>
+<body>
+    <h1>meu primeiro site com PHP</h1>
+    <p>
+        <?php
+            echo "ola mundo \u{1F30E}"; // ou print "ola mundo"
+        ?>
+    </p>
+</body>
+</html>
