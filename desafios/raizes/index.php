6150c7ab1792350fcf9557a28abcbe3b0440fe16
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2023-07-05 16:07:15 -0300

    melhorei e consertei alguns bugs no código

--- desafios/raizes/index.php
+++ desafios/raizes/index.php
@@ -9,14 +9,14 @@
 <body>
     <h1>cálculo de raízes</h1>
 
-    <? $num = $_GET["num"] ?? 0 ?>
+    <?php $num = $_GET["num"] ?? 1?>
 
     <main>
         <h2>insira um número</h2>
 
-        <form action="<? echo $_SERVER["PHP_SELF"] ?>" method="get">
+        <form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
             <label for="num">número</label>
-            <input type="number" name="num" id="num" value="<?=$num?>">
+            <input type="number" name="num" id="num" value="<?=$num?>" min="1">
 
             <input type="submit" value="calcular raíz">
         </form>
@@ -26,17 +26,29 @@
         <h2>resultado</h2>
 
         <?php
-            echo "<p>analisando o número $num, temos:</p>";
+        if ($num < 1):
+        ?>
+        <p>não foi possível analisar o número "<strong><?=$num?></strong>" pois ele é menor que 1</p>
 
+        <?php
+        elseif (!is_numeric($num)):
+        ?>
+        <p>não foi possível analisar o número "<strong><?=$num?></strong>" pois ele não é um número</p>
+
+        <?php
+        else:
             $raizQuadra = number_format(sqrt($num), 2);
             $raizCubica = number_format($num ** (1/3), 2);
+        ?>
+        <p>analisando o número <strong><?=$num?></strong>, temos:</p>
 
-            echo <<< LISTA
-                <ul>
-                    <li>a sua raíz quadrada é $raizQuadra</li>
-                    <li>a sua raíz cúbica é $raizCubica</li>
-                </ul>
-            LISTA;
+        <ul>
+            <li>a sua raíz quadrada é <?=$raizQuadra?></li>
+            <li>a sua raíz cúbica é <?=$raizCubica?></li>
+        </ul>
+
+        <?php
+        endif;
         ?>
     </section>
 </body>
